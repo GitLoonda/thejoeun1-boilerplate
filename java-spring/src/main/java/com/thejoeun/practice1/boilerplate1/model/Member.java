f04aac2403c254af1be7091c80845ca89bc30cb1
GitLoonda <wjddlsxo97@naver.com>
2023-07-24 18:23:22 +0900

    4. jwt & oauth configuration setting

--- java-spring/src/main/java/com/thejoeun/practice1/boilerplate1/model/Member.java
+++ java-spring/src/main/java/com/thejoeun/practice1/boilerplate1/model/Member.java
@@ -1,10 +1,8 @@
 package com.thejoeun.practice1.boilerplate1.model;
 
+import com.thejoeun.practice1.boilerplate1.model.enums.Authority;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.apache.ibatis.annotations.Mapper;
 
 @Entity
@@ -13,15 +11,49 @@ import org.apache.ibatis.annotations.Mapper;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
     private Long id;
 
     @Column(nullable = false)
     private String email;
 
+    //    @Column(nullable = false)
     private String password;
 
+    //    @Column(nullable = false)
     private String nickname;
+
+    private String provider;
+
+    @Column(length = 2000)
+    private String accessToken;
+
+    private Long accessTokenExpireIn;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+
+    public Member update(String nickname, String email) {
+        this.nickname = nickname;
+        this.email = email;
+        return this;
+    }
+
+    public Member update(
+//            String name,
+            String accessToken,
+            Long accessTokenExpireIn,
+            String email
+    ) {
+//        this.name = name;
+        this.accessToken = accessToken;
+        this.accessTokenExpireIn = accessTokenExpireIn;
+        this.email = email;
+        return this;
+    }
 }
