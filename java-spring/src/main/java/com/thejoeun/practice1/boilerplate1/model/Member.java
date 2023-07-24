b0bf1afcbf3207b781995f8a964d66ca42769b0f
GitLoonda <wjddlsxo97@naver.com>
2023-07-24 14:37:24 +0900

    3. Mybatis & JPA Connection setting / test

---
+++ java-spring/src/main/java/com/thejoeun/practice1/boilerplate1/model/Member.java
@@ -0,0 +1,27 @@
+package com.thejoeun.practice1.boilerplate1.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.ibatis.annotations.Mapper;
+
+@Entity
+@Mapper
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String password;
+
+    private String nickname;
+}
---
+++ java-spring/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO member(`id`, `email`, `password`, `nickname`) VALUES (1, "test1@test.com", "1q2w3e4r", "test1");
+INSERT INTO member(`id`, `email`, `password`, `nickname`) VALUES (2, "test2@test.com", "1q2w3e4r!", "test2");
+INSERT INTO member(`id`, `email`, `password`, `nickname`) VALUES (3, "test3@test.com", "1q2w3e4r@", "test3");
