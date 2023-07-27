b9ea570d87b30ab98682141bc56ae57b60d017eb
GitLoonda <wjddlsxo97@naver.com>
2023-07-27 14:57:23 +0900

    7. react frontend build pages update

--- react-frontend/src/components/SignUpForm.jsx
+++ react-frontend/src/components/SignUpForm.jsx
@@ -1,18 +1,17 @@
 import React, { useState, useRef, useContext, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function SignUpForm () {
+
   return (
     <section className="d-flex vh-100" style={{ backgroundColor: "rgb(33,37,41)" }}>
       <div className="container-fluid row justify-content-center align-content-center">
-        <div className="card bg-dark" style={{borderRadius: "1rem"}}>
+        <div className="card bg-dark" style={{borderRadius: '1rem'}}>
           <div className="card-body p-5 text-center">
-            <h2 className="text-white">SIGN UP</h2>
-            <p className="text-white-50 mt-2 mb-5">서비스 사용을 위한 회원 가입</p>
-
-            <div className = "mb-2">
+            <h2 className="text-white">회원가입</h2>
+            <p className="text-white-50 mt-2 mb-5">서비스 사용을 위해서 회원가입을 해주세요</p>
+            <div className="mb-2">
               <form method="POST">
-                {/* <!-- 토큰을 추가하여 CSRF 공격 방지 --> */}
-                <input type="hidden" />
                 <div className="mb-3">
                   <label className="form-label text-white">Email address</label>
                   <input type="email" className="form-control" name="email" />
@@ -21,15 +20,18 @@ function SignUpForm () {
                   <label className="form-label text-white">Password</label>
                   <input type="password" className="form-control" name="password" />
                 </div>
-
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <div className="mb-3">
+                  <label className="form-label text-white">Nickname</label>
+                  <input type="text" className="form-control" name="nickname" />
+                </div>
+                <button type="submit" className="btn btn-primary">회원가입</button>
               </form>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default SignUpForm;
